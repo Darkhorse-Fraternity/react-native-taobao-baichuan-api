c759a7ca1a4be58072b91ee4cfa7774b91fee695
tonyYo <420156367@qq.com>
2017-02-10 16:51:18 +0800

    no message

--- ios/React-Native-Taobao-Baichuan-Api/React_Native_Taobao_Baichuan_Api.m
+++ ios/React-Native-Taobao-Baichuan-Api/React_Native_Taobao_Baichuan_Api.m
@@ -19,17 +19,33 @@ RCT_EXPORT_MODULE(React_Native_Taobao_Baichuan_Api);
 RCT_EXPORT_METHOD(jump:(NSString *)itemId)
 {
     
-    [self itemDetailPage: itemId];
+    [self itemDatailPageWithItem: itemId];
+    
+}
+RCT_EXPORT_METHOD(present:(NSString *)url)
+{
+    
+    [self itemDetailPageWithURL: url];
     
 }
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
--(void)itemDetailPage: (NSString *) item{
-    
+
+-(void)itemDatailPageWithItem:(NSString *)item{
     NSString *itemID = item;
     id<AlibcTradePage> page = [AlibcTradePageFactory itemDetailPage: itemID];
+    [self itemDetailPage:page];
+}
+
+-(void)itemDetailPageWithURL:(NSString *)url {
+    id<AlibcTradePage> page = [AlibcTradePageFactory page: url];
+    [self itemDetailPage:page];
+}
+
+-(void)itemDetailPage: (id) page{
+    
     //淘客信息
     AlibcTradeTaokeParams *taoKeParams=[[AlibcTradeTaokeParams alloc] init];
     taoKeParams.pid= nil;
@@ -37,27 +53,27 @@ RCT_EXPORT_METHOD(jump:(NSString *)itemId)
     AlibcTradeShowParams* showParam = [[AlibcTradeShowParams alloc] init];
     showParam.openType = ALiOpenTypeAuto;
     showParam.isNeedPush = NO;
- 
+    
     //UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
     AppDelegate *share = (AppDelegate *)[UIApplication sharedApplication].delegate;
     UINavigationController *nav = (UINavigationController *) share.window.rootViewController;
-
+    
     NSInteger ret = [[AlibcTradeSDK sharedInstance].tradeService
-     show: nav
-     page: page
-     showParams: showParam
-     taoKeParams: taoKeParams
-     trackParam:nil
-     tradeProcessSuccessCallback:^(AlibcTradeResult * __nullable result) {
-            NSLog(@"%@", result);
-        }
-    tradeProcessFailedCallback:^(NSError * __nullable error) {
-            NSLog(@"%@", error);
-        }
-    ];
+                     show: nav
+                     page: page
+                     showParams: showParam
+                     taoKeParams: taoKeParams
+                     trackParam:nil
+                     tradeProcessSuccessCallback:^(AlibcTradeResult * __nullable result) {
+                         NSLog(@"%@", result);
+                     }
+                     tradeProcessFailedCallback:^(NSError * __nullable error) {
+                         NSLog(@"%@", error);
+                     }
+                     ];
     //返回1,说明h5打开,否则不应该展示页面
     if (ret == 1) {
-//        [self.navigationController pushViewController:view animated:YES];
+        //        [self.navigationController pushViewController:view animated:YES];
     }
     return ;
 }
